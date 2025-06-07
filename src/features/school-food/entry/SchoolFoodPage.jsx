ea83ed9cd8b6a8cd0142f0ac34f8e7d5b5f65d9c
DongGyun <wooheuy5752@gmail.com>
2025-06-07 15:57:28 +0900

    test: cors 테스트

--- src/features/school-food/entry/SchoolFoodPage.jsx
+++ src/features/school-food/entry/SchoolFoodPage.jsx
@@ -1,17 +1,31 @@
+import { useEffect, useState } from 'react'
+
+import { instance } from '@/api/client'
 import { FloatingButton, MainLayout } from '@/components'
 
 import { SchoolFoodOfficialLink } from '../ui/SchoolFoodOfficialLink'
-import { SchoolFoodReviewList } from '../ui/SchoolFoodReviewList'
 import styles from './SchoolFoodPage.module.css'
 
 export const SchoolFoodPage = () => {
+  const [reviewList, setReviewList] = useState([])
+
+  const fetchReviewList = async () => {
+    const reviewList = await instance.get(`/reviews/page/${1}`)
+    console.log(reviewList)
+    setReviewList(reviewList)
+  }
+
+  useEffect(() => {
+    fetchReviewList()
+  }, [])
+
   return (
     <MainLayout title='학식 정보'>
       <div className={styles.container}>
         <SchoolFoodOfficialLink />
         <p className={styles.label}>학식 리뷰 확인해 보세요!</p>
 
-        <SchoolFoodReviewList />
+        {/* <SchoolFoodReviewList reviewList={reviewList} /> */}
       </div>
       <FloatingButton to={'/school-food/review'} />
     </MainLayout>
